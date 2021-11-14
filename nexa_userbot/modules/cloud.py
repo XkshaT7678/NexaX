f1882f99716b23501a5abdf63a1cbab0e1487036
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-11-14 15:14:48 +0530

    Update cloud.py

--- nexa_userbot/modules/cloud.py
+++ nexa_userbot/modules/cloud.py
@@ -2,7 +2,6 @@
 import os
 
 from pyrogram.types import Message
-from gofile2 import Async_Gofile
 
 from nexa_userbot import NEXAUB, CMD_HELP
 from nexa_userbot.core.main_cmd import nexaub_on_cmd, e_or_r
