407ef775fae172a9c3c74da0834dba2c84390d3d
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-11-14 14:27:10 +0530

    Update startup_checks.py

--- nexa_userbot/core/startup_checks.py
+++ nexa_userbot/core/startup_checks.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2021 Itz-fork
-# Part of: Nexa-Userbot
+
 import asyncio
 from pyrogram.errors import YouBlockedUser
 from nexa_userbot import NEXAUB
@@ -13,22 +12,18 @@ async def check_or_set_log_channel():
         if log_channel_id:
             return [True, log_channel_id]
         else:
-            log_channel = await NEXAUB.create_channel(title="Nexa Userbot Logs", description="Logs of your Nexa Userbot")
+            log_channel = await NEXAUB.create_channel(title="Nexa Userbot Logs", description="Logs Of NEXA-UserBot. Don't delete this channel.")
             welcome_to_nexaub = f"""
-**Welcome to Nexa Userbot**
-Thanks for trying Nexa Userbot. If you found any error, bug or even a Feature Request please report it at **@NexaUB_Support**
+**Heya! Myself NEXA-UserBot, A powerful Userbot.**
+ Head To **@Nexa2Support** for help.
 
-**⌲ Quick Start,**
-If you don't know how to use this Userbot please send `{Config.CMD_PREFIX}help` in any chat. It'll show all plugins your userbot has. You can use those plugin names to get info about how to use it. Also check out [Docs](https://nexa-userbot.netlify.app)
-
-
- **~ Nexa Userbot Authors**"""
+**~ Nexa Userbot **"""
             log_channel_id = log_channel.id
             await set_log_channel(log_channel_id)
             await NEXAUB.send_message(chat_id=log_channel_id, text=welcome_to_nexaub, disable_web_page_preview=True)
             return [True, log_channel_id]
     except Exception as e:
-        print(f"Error \n\n{e} \n\nPlease check all variables and try again! \nReport this with logs at @NexaUB_Support if the problem persists!")
+        print(f"Error \n\n{e} \n\nPlease check all variables and try again! \nReport this with logs at @Nexa2_Support if the problem persists!")
         exit()
 
 
