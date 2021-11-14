51facf86010ff6ad49af3f75cf029c25fc4ea697
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-11-14 14:36:25 +0530

    Update pyro_str_gen.py

--- pyro_str_gen.py
+++ pyro_str_gen.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2021 Itz-fork
-# Part of: Nexa-Userbot
+
 
 import asyncio
 import tgcrypto
@@ -8,7 +7,7 @@ from pyrogram import Client
 print("""
 || Nexa Userbot ||
 
-Copyright (c) 2021 Itz-fork
+Copyright (c) ComradeDear
 """)
 
 async def pyro_str():
@@ -18,7 +17,7 @@ async def pyro_str():
     async with Client(":memory:", api_id, api_hash) as NEXAUB:
         pyro_session = await NEXAUB.export_session_string()
         session_msg = await NEXAUB.send_message("me", f"`{pyro_session}`")
-        await session_msg.reply_text("Successfully Generated String Session! Thanks for trying [Nexa Userbot](https://github.com/Itz-fork/Nexa-Userbot) \n\n**Join @NexaBotsUpdates**", disable_web_page_preview=True)
+        await session_msg.reply_text("Successfully Generated String Session! Thanks for trying [Nexa Userbot](https://github.com/ComradeDear/nexa2.0) \n\n**Join @Nexa2channel**", disable_web_page_preview=True)
         print("\nString Session has been sent to your saved messages. Please check it. Thank You!\n")
 
 if __name__ == "__main__":
